849dff2b09fa6eb3e800b28531b096d329a4ac7f
abdulqadir <abdulqadirbinmuslim@gmail.com>
2024-08-28 01:12:33 +0100

    fixed some issues

---
+++ diaspora/env.py
@@ -0,0 +1,1 @@
+django_key = "django-insecure-syo5f%xs@cm1&ilr8^uba5xiemfro9a+3ba&0rcyqvj03sk0q&"
--- diaspora/settings.py
+++ diaspora/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-from .env import django_key
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = django_key
+SECRET_KEY = os.environ.get("django_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

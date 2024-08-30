cc5dc3c8b4d2955fc167d350bf5654008ccba011
abdulqadir <abdulqadirbinmuslim@gmail.com>
2024-08-30 17:58:38 +0100

    updated django

--- diaspora/settings.py
+++ diaspora/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("django_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# ["127.0.0.1", ".vercel.app"]
-ALLOWED_HOSTS = []
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 
 # Application definition
